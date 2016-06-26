6f5f95d1f37f34ef21974ff38446f2b7b361363d
Juan Caicedo <jjcc1421@gmail.com>
2016-06-26 17:11:01 -0500

    Add PSE transaction request class

--- packages/jjcaicedo/placeToPay/src/Models/PSETransactionRequest.php
+++ packages/jjcaicedo/placeToPay/src/Models/PSETransactionRequest.php
@@ -71,5 +71,142 @@ class PSETransactionRequest
         $this->additionalData = $additionalData;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getBankCode()
+    {
+        return $this->bankCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBankInterface()
+    {
+        return $this->bankInterface;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReturnURL()
+    {
+        return $this->returnURL;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTotalAmount()
+    {
+        return $this->totalAmount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTaxAmount()
+    {
+        return $this->taxAmount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDevolutionBase()
+    {
+        return $this->devolutionBase;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTipAmount()
+    {
+        return $this->tipAmount;
+    }
+
+    /**
+     * @return \JJCaicedo\PlaceToPay\Models\Person
+     */
+    public function getPayer()
+    {
+        return $this->payer;
+    }
+
+    /**
+     * @return \JJCaicedo\PlaceToPay\Models\Person
+     */
+    public function getBuyer()
+    {
+        return $this->buyer;
+    }
+
+    /**
+     * @return \JJCaicedo\PlaceToPay\Models\Person
+     */
+    public function getShipping()
+    {
+        return $this->shipping;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdditionalData()
+    {
+        return $this->additionalData;
+    }
+
+
 
 }
